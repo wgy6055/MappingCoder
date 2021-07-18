59fa53280d21d8c83e587e68af74102dac28c0c4
Wang Guanyu <guanyu4real@gmail.com>
2021-07-18 19:10:13 +0800

    Initial Commit

---
+++ MappingCoder/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MappingCoder
+//
+//  Created by Wang Guanyu on 2021/7/18.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
